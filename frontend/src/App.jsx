830d1778f5aed054efbff8d780f15c43baeb3535
fgrebows <fgrebows@cisco.com>
2018-11-17 15:36:01 -0800

    Added react structure.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import '../node_modules/normalize.css/normalize.css';
+
+import './App.scss';
+
+class App extends Component {
+
+    render() {
+        return (
+            <span>Hello Brychan Hello</span>
+        );
+    }
+}
+
+export default App;
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,9 +2,9 @@ var Encore = require('@symfony/webpack-encore');
 
 Encore
     // directory where compiled assets will be stored
-    .setOutputPath('src/assets/build/')
+    .setOutputPath('public/build/')
     // assets path used by the web server to access the output path
-    .setPublicPath('/build')
+    .setPublicPath('/build/')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
@@ -17,7 +17,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    .addEntry('app', './src/app.js')
+    .addEntry('app', './src/index.jsx')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
