99a3d75a07cf30f83eb2e7dad48c4adb48b12d1c
fgrebows <fgrebows@cisco.com>
2018-11-17 16:09:43 -0800

    Added canvas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,13 @@ import React, {Component} from 'react';
 import '../node_modules/normalize.css/normalize.css';
 
 import './App.scss';
+import Canvas from './Canvas';
 
 class App extends Component {
 
     render() {
         return (
-            <span>Hello Brychan Hello</span>
+            <Canvas />
         );
     }
 }
---
+++ frontend/src/Canvas.jsx
@@ -0,0 +1,23 @@
+import React, {Component} from 'react';
+
+import './Canvas.scss';
+
+export default class Canvas extends Component {
+
+    componentDidMount() {
+        const canvas = this.refs.canvas;
+        const ctx = canvas.getContext("2d");
+
+        ctx.fillStyle = "#FF0000";
+        ctx.fillRect(0,0,150,75);
+
+    }
+
+    render() {
+        return(
+            <div>
+                <canvas className={"canvas"} ref={"canvas"} width={640} height={425} />
+            </div>
+        )
+    }
+}
